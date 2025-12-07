2df4fa3336e4d5683bd6ba8ebc2c66a038ff83d5
ruhan <mdruhanahmed49@gmail.com>
2025-12-07 22:06:55 +0600

    footer section style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 
-export default function Footer(){
-return (
-<footer className="py-6 bg-gray-400 flex">
-<div className="container mx-auto px-4 text-center">
-<p>© {new Date().getFullYear()} Ruhan Ahmed. All rights reserved.</p>
-</div>
-<div className='text-2xl '>
-<a href="" className='text-white bg-blue-500'><i class="fa-brands fa-github"></i></a>
-<a href="" className='text-white bg-blue-500 mx-2'><i class="fa-brands fa-instagram"></i></a>
-<a href="" className='text-white bg-blue-500'><i class="fa-brands fa-facebook"></i></a>
-</div>
-</footer>
-)
+export default function Footer() {
+    return (
+        <footer className="py-6 bg-gray-400 flex">
+            <div className="container mx-auto px-4 text-center">
+                <p>© {new Date().getFullYear()} <span className='text-orange-300'>Ruhan Ahmed.</span> All rights reserved.</p>
+            </div>
+            <div className='text-2xl '>
+                <a href="https://github.com/ruhanahmed01706" className='text-white bg-gray-700 rounded-full'><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/md-ruhan-ahmed-21a692390/" className='text-white bg-blue-700 mx-2'><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://www.facebook.com/md.ruhan.ahmed.533192" className='text-white bg-blue-700 rounded-full'><i class="fa-brands fa-facebook"></i></a>
+            </div>
+        </footer>
+    )
 }
